625abf7c3669b669a54796971bc4797ed7bbf461
Kevin Wong <kevinwong@pursuit.org>
2019-10-21 20:33:14 -0400

    Problems 1 & 2 completed

--- classes_lab.js
+++ classes_lab.js
@@ -6,18 +6,47 @@
 
 // a. Write a class called `Person` that has 3 properties: a first name, a last name and a middle name. Create 2 instances of a `Person`. Print one of their first names.
 class Person {
-    
+    constructor(firstName, lastName, middleName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+    }
+
+    fullName(){
+        return  `${this.firstName} ${this.middleName} ${this.lastName}`;
+    }
+
 }
+let person1 = new Person("kevin","wong","j");
+let person2 = new Person("farrah","rios","isabelle");
+console.log(person2.firstName);
 
 // b. Write a method in `Person` called `fullName` that will return a formatted string of an instance's full name. Call this method on both the instances you created in part a.
-
+console.log(person1.fullName());
+console.log(person2.fullName());
 
 // ## Question 2
 
 // a. Create a class called `Book` that has properties `title`, `author` and `rating`. Create some instances of `Book`.
+class Book {
+    constructor (title, author, rating){
+        Object.assign(this, {title, author, rating});
+    }
 
+    isGood(){
+        if(this.rating >= 7){
+          return true;
+        } else {
+          return false;
+        }
+    }
+}
+let book1 = new Book("Sapiens: A Brief History of Humankind","Yuval Noah Harari", 9.2);
+let book2 = new Book("Radical Candor: How to Get What You Want by Saying What You Mean","Kim Scott", 8.4);
 
 // b. Add a method to `Book` called `isGood` that returns `true` if its rating is greater than or equal to 7
+console.log(book1.isGood());
+console.log(book2.isGood());
 
 // ## Question 3
 
